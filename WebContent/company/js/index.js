36f7b98d887305063b72b478724b8bd17aad7cf8
taolichao <xvhandong@sina.com>
2016-09-09 17:33:59 +0800

    首页

--- WebContent/company/js/index.js
+++ WebContent/company/js/index.js
@@ -9,10 +9,34 @@ $(function(){
 		success : function(resp){
 			var data = eval('('+resp+')');
 			$('#hmPage-Cont').html('');
-			$.each(data.root,function(i,item){
+			
+			//首页
+			$.each(data.root[0],function(i,item){
 				$('#hmPage-Cont').append(item.contentdetail);
 				$('#hmPage-Cont .swiper-slide:eq('+i+')').css('background'," url('"+item.contentname+"') 50% 35% ");
 			});
+			
+			//关于
+			$('#guanyu-cont').css('background'," url('"+data.root[1][0].contentback+"') 50% 60% ");
+			$('#guanyu-cont .nth1').append(data.root[1][0].contentdetail);
+			$('#guanyu-cont .nth3').append(data.root[1][1].contentdetail);
+			$.each(data.root[2],function(i,item){
+				var newsT = item.contentdetail.substring(24,item.contentdetail.indexOf('<span>'));
+				var newsCont = item.contentdetail.substring(item.contentdetail.indexOf('<p>')+3,item.contentdetail.indexOf('</p>'));
+				$('#guanyu-cont .nth4').append('<ul><li><span>'+newsT+'</span><br>'+newsCont+
+						'<a href="newslist.html">查看详情 > </a></li></ul>');
+			});
+			
+			//服务
+			$('#sever-cont').css('background'," url("+data.root[3][0].contentback+") 50% bottom");
+			$.each(data.root[3],function(i,item){
+				$('#sever-cont .items').append('<li class="pc">'+item.contentdetail+'</li>');
+			});
+			//提交"入驻"表单
+			$('.cd-buttons .confirm-btn').click(subSQRZfm);
+			
+			
+			//特效
 			if (window.location.host > 0) {
 		        $("section").remove();
 		        return;
@@ -27,3 +51,50 @@ $(function(){
 		}
 	});
 });
+
+function subSQRZfm(){
+	var strJson = '[{';
+	$('#sqrz-fm input').each(function(i,item){
+		strJson += '"'+item.name+'":"'+item.value+'",';
+	});
+	strJson = strJson.substring(0,strJson.length-1) + '}]';
+	$.ajax({
+		url:"IntentioncAction.do?method=insAll",
+		type:"post",
+		data:{
+			json:strJson
+		},
+		success:function(resp){
+			var data = eval('('+resp+')');
+			if(data.msg == '操作成功'){
+				alert('入驻申请已提交!');
+			} else {
+				alert('申请提交失败!');
+			}
+		},
+		error : function(resp){
+			var data = eval('('+resp+')');
+			alert(data.msg);
+		}
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
