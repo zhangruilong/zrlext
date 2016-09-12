92f37bbbc934a5773bd11e5ca5db2e1e7c066df2
taolichao <xvhandong@sina.com>
2016-09-12 11:09:52 +0800

    首页

--- WebContent/company/js/index.js
+++ WebContent/company/js/index.js
@@ -13,7 +13,7 @@ $(function(){
 			//首页
 			$.each(data.root[0],function(i,item){
 				$('#hmPage-Cont').append(item.contentdetail);
-				$('#hmPage-Cont .swiper-slide:eq('+i+')').css('background'," url('"+item.contentname+"') 50% 35% ");
+				$('#hmPage-Cont .swiper-slide:eq('+i+')').css('background'," url('"+item.contentback+"') 50% 35% ");
 			});
 			
 			//关于
@@ -32,9 +32,15 @@ $(function(){
 			$.each(data.root[3],function(i,item){
 				$('#sever-cont .items').append('<li class="pc">'+item.contentdetail+'</li>');
 			});
+			
 			//提交"入驻"表单
 			$('.cd-buttons .confirm-btn').click(subSQRZfm);
 			
+			//方案
+			$.each(data.root[4],function(i,item){
+				$('#product-cont .'+item.contentcode.substring(1,3)+' .he_ZoomInImg').append('<img class="he_ZoomInImg_img" src="'+item.contentback+
+						'" width="100%" height="100%" alt=""><div class="he_ZoomInImg_caption">'+item.contentdetail+'</div>');
+			});
 			
 			//特效
 			if (window.location.host > 0) {
@@ -51,7 +57,7 @@ $(function(){
 		}
 	});
 });
-
+//提交"入驻"表单
 function subSQRZfm(){
 	var strJson = '[{';
 	$('#sqrz-fm input').each(function(i,item){
