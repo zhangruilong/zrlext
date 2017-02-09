780132267d78e4b0f9b29f04b0670762b51cc7dc
zhangruilong <zhangruilong@192.168.31.153>
2017-02-09 13:37:12 +0800

    mac

---
+++ WebContent/zrlextpages/mac/js/data.js
@@ -0,0 +1,375 @@
+﻿DATA={
+	menu:[{//一级菜单
+		menuid:'m001',
+		name:'主页',
+		code:'m001',
+		icon:'images/home.png'
+	},{
+		menuid:'m002',
+		name:'邮件',
+		code:'m002',
+		icon:'images/email.png'
+	},/*{
+		menuid:'m003',
+		name:'文件夹',
+		code:'m003',
+		icon:'images/portfolio.png'
+	},*/{
+		menuid:'m003',
+		name:'音乐',
+		code:'m003',
+		icon:'images/music.png'
+	},{
+		menuid:'m004',
+		name:'视频',
+		code:'m004',
+		icon:'images/video.png'
+	},/*{
+		menuid:'m006',
+		name:'备忘录',
+		code:'m006',
+		icon:'images/history.png'
+	},{
+		menuid:'m007',
+		name:'日历',
+		code:'m007',
+		icon:'images/calendar.png'
+	},*/{
+		menuid:'m005',
+		name:'RSS',
+		code:'m005',
+		icon:'images/rss.png'
+	}],
+	app:{//桌面1主页
+		'readGod':{
+			appid:'2534',
+			icon:'readGod.png',
+			name:'读览天下',
+			url:'http://www.baidu.com/',
+			sonMenu:"[{"+
+				"'appid':'8856',"+
+				"'icon':'sosomap.png',"+
+				"'name':'搜搜地图',"+
+				"'url':'http://www.baidu.com/'"+
+			"},{"+
+				"appid:'8857',"+
+				"icon:'time.png',"+
+				"name:'时钟',"+
+				"url:'http://www.baidu.com/'"+
+			"},{"+
+				"appid:'8858',"+
+				"icon:'jinshan.png',"+
+				"name:'金山快盘',"+
+				"url:'http://www.baidu.com/'"+
+			"}]",
+			asc :1
+		},
+		'sosomap':{
+			appid:'42',
+			icon:'sosomap.png',
+			name:'搜搜地图',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'jinshan':{
+			appid:'8992',
+			icon:'jinshan.png',
+			name:'金山快盘',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'kaikai':{
+			appid:'2250',
+			icon:'kaikai.png',
+			name:'开开',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'Pixlr':{
+			appid:'64',
+			icon:'pixlr.png',
+			name:'Pixlr',
+			url:'msg.html',
+			sonMenu:"[]",
+			asc:5
+		},
+		'weather':{
+			appid:'18',
+			icon:'5.png',
+			name:'天气',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc:6
+		},
+		'Clock':{
+			appid:'20',
+			icon:'time.png',
+			name:'时钟',
+			url:'http://www.baidu.com/',
+			asc:7
+		},
+		'yule':{
+			appid:'514',
+			icon:'folder_o.png',
+			name:'娱乐',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc:8
+		},
+		//桌面2关于我们
+		'friendnear':{
+			appid:'31',
+			icon:'friendnear.png',
+			name:'好友近况',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'friendgroup':{
+			appid:'32',
+			icon:'friendgroup.png',
+			name:'好友管理',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'mangguo':{
+			appid:'3401',
+			icon:'mangguo.png',
+			name:'芒果旅游',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'tuanmap':{
+			appid:'2527',
+			icon:'tuanmap.png',
+			name:'团购地图',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'fastsearch':{
+			appid:'3693',
+			icon:'fastsearch.png',
+			name:'快递查询',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'bianqian':{
+			appid:'10',
+			icon:'bianqian.png',
+			name:'便签',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :6
+		},
+		'wangdesk':{
+			appid:'13',
+			icon:'wangdesk.png',
+			name:'网络硬盘',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :7
+		},
+		//桌面3生活频道（音视书）
+		'baidu':{
+			appid:'3402',
+			icon:'ic_launcher.png',
+			name:'百度随心听',
+			url:'http://fm.baidu.com/?embed=ps&amp;bd_user=1175989175&amp;bd_sig=fc705946f7807752a44643f7b49f0bf3&amp;canvas_pos=platform/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'douban':{
+			appid:'21',
+			icon:'douban.png',
+			name:'豆瓣FIM',
+			url:'http://douban.fm/partner/qq_plus',
+			sonMenu:"[]",
+			asc :2
+		},
+		'xiami':{
+			appid:'10000',
+			icon:'xiami.png',
+			name:'虾米电台',
+			url:'http://www.xiami.com/song/play?ids=/song/playlist/id/576498/type/1',
+			sonMenu:"[]",
+			asc :3
+		},
+		'qqmusic':{
+			appid:'10001',
+			icon:'qqmusic.png',
+			name:'QQ音乐',
+			url:'http://y.qq.com/webplayer/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'kugou':{
+			appid:'10002',
+			icon:'kugou.png',
+			name:'酷狗',
+			url:'http://web.kugou.com/default.html',
+			sonMenu:"[]",
+			asc :5
+		},
+		'kuwo':{
+			appid:'10003',
+			icon:'kuwo.png',
+			name:'酷我',
+			url:'http://player.kuwo.cn/webmusic/play',
+			sonMenu:"[]",
+			asc :6
+		},
+		//桌面4项目简介
+		'qidianzhongwen':{
+			appid:'2528',
+			icon:'qidianzhongwen.png',
+			name:'起点中文',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'qqread':{
+			appid:'45',
+			icon:'qqread.png',
+			name:'奥尔阅读',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'kxjy':{
+			appid:'2535',
+			icon:'kxjy.png',
+			name:'奥尔交友',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'musicbox':{
+			appid:'56',
+			icon:'musicbox.png',
+			name:'音乐盒子',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'vadio':{
+			appid:'15',
+			icon:'vadio.png',
+			name:'奥尔视频',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'leshi':{
+			appid:'3148',
+			icon:'leshi.png',
+			name:'乐视网',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :6
+		},
+		//桌面5
+		'doudizhu':{
+			appid:'48',
+			icon:'doudizhu.png',
+			name:'欢乐斗地主',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'3366':{
+			appid:'49',
+			icon:'3366.png',
+			name:'3366',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'qqbaby':{
+			appid:'26',
+			icon:'qqbaby.png',
+			name:'QQ宝贝',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'folder_o':{
+			appid:'1000',
+			icon:'bianqian.png',
+			name:'便签',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :4
+		}
+	},
+	sApp:{//侧边栏应用
+		'appmarket':{
+			appid:'1',
+			icon:'appmarket.png',
+			name:'应用市场',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'qq':{
+			appid:'2',
+			icon:'big.png',
+			name:'QQ',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'weibo':{
+			appid:'3',
+			icon:'weibo.png',
+			name:'微博',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'mail':{
+			appid:'4',
+			icon:'mail.png',			
+			name:'邮箱',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'zone':{
+			appid:'5',
+			icon:'zone.png',
+			name:'空间',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'internet':{
+			appid:'6',
+			icon:'internet.png',//QQ浏览器
+			name:'浏览网页',
+			url:'http://www.baidu.com/',
+			sonMenu:"[]",
+			asc :6
+		}
+	}
+};
+ops = {//向桌面添加应用
+	Icon1:['readGod','sosomap','jinshan','kaikai','Pixlr','yule','weather','Clock'],
+	Icon2:['friendnear','friendgroup','mangguo','tuanmap','fastsearch','bianqian','wangdesk'],
+	Icon3:['baidu','douban','xiami','qqmusic','kugou','kuwo'],
+	Icon4:['qidianzhongwen','qqread','kxjy','musicbox','vadio','leshi'],
+	Icon5:['doudizhu',	'3366',	'qqbaby','folder_o']
+	/*Icon6:['3366','friendnear','yule','kaikai','Clock'],
+	Icon7:['friend','sosomap','xiami','folder_o','kxjy'],
+	Icon8:['friend','mangguo']*/
+};
+//初始化左边快捷菜单
+var leftMenu = new Array(['appmarket','qq','weibo','mail','internet','zone']);
+
+
